440b5055af3474b2a739dc3b11940156a5c56bf1
victormagueta <vmagueta7@gmail.com>
2021-03-19 21:06:24 -0300

    Correção de Exercício 012.py

--- Exercícios Curso em Vídeo/Exercício 012.py
+++ Exercícios Curso em Vídeo/Exercício 012.py
@@ -3,4 +3,4 @@
 price = float(input('Quanto custa o produto? '))
 discount = float(input('Qual será o percentual de desconto aplicado? '))
 
-print(f'''O produto que custava R$ {price:.2f}, com o desconto de {discount} % aplicado, sairá por R$ {price - (price * (1 / discount)):.2f}.''')
+print(f'''O produto que custava R$ {price:.2f}, com o desconto de {discount} % aplicado, sairá por R$ {price - (price * (discount / 100)):.2f}.''')
