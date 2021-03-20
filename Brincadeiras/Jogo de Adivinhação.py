43837b74bc1f341fad93dfc36040652536a19bf3
victormagueta <vmagueta7@gmail.com>
2021-03-20 15:45:17 -0300

    Jogo de adivinhação finalizado

--- Brincadeiras/Jogo de Adivinhação.py
+++ Brincadeiras/Jogo de Adivinhação.py
@@ -1,10 +1,19 @@
-# Estou pensando em um número de 1 a 10. Em qual número estou pensando? #
+# Estou pensando em um número de 1 a 5. Em qual número estou pensando? #
+import random
 
-
-from random import randint
-
+print('Estou pensando em um número de 1 a 5. Consegue adivinhar qual é? ')
 num = int(input('Digite o número que estou pensando: '))
 
-lista = list(range(1, 11))
-pens = lista.shuffle(lista)
-print(lista)
+lista = list(range(1, 5))
+random.shuffle(lista)
+n = lista.pop()
+
+if 1 <= num <= 5:
+    if n == num:
+        print(f'Parabéns, você acertou! Você disse {num} e eu pensei no número {n}.')
+    elif n != num:
+        print(f'Que pena, você errou! Você disse {num} e eu pensei no número {n}.')
+    else:
+        print('Comando Inválido')
+else:
+    print(f'O número {num} que você digitou não está entre os números 1 e 5. Tente novamente!')
